5c2ab5be650a19899a4776bbda0760a53a9f6751
RomaChumak <romachumak355@gmail.com>
2023-08-29 15:17:08 +0300

    Update Layout.styled.js

--- src/components/Layout.styled.js
+++ src/components/Layout.styled.js
@@ -3,6 +3,7 @@ export const Layout = styled.div`
     display: flex;
     align-items: center;
     gap: 60px;
+    justify-content: center;
     max-width: 1440px;
     margin: 0px auto;
     padding: 0px 15px;
