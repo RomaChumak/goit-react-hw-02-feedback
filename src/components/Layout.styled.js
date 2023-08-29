50ef73ac63a3e44c293a94926bf4f6e99a59d0bb
RomaChumak <romachumak355@gmail.com>
2023-08-29 15:14:57 +0300

    2

---
+++ src/components/Layout.styled.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+export const Layout = styled.div`
+    display: flex;
+    align-items: center;
+    gap: 60px;
+    max-width: 1440px;
+    margin: 0px auto;
+    padding: 0px 15px;
+ `
